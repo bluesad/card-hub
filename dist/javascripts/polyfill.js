0d52caa9ba86ad93e34da0e010ab84f86b19330c
ishida <webmaster@joe1cafe.com>
2020-12-27 00:28:28 +0800

    update pipeline

--- dist/javascripts/polyfill.js
+++
@@ -1,8 +0,0 @@
-navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
-   getUserMedia: function(c) {
-     return new Promise(function(y, n) {
-       (navigator.mozGetUserMedia ||
-        navigator.webkitGetUserMedia).call(navigator, c, y, n);
-     });
-   }
-} : null);
