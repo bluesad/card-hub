dc2a5e8332ed945cfad5b7312a3734e8d3600506
Zhitao Shen <shenzt@gmail.com>
2020-12-28 02:13:06 +0800

    fix

--- public/javascripts/cv-app.js
+++ public/javascripts/cv-app.js
@@ -1,31 +1,57 @@
 function Cov() {
     let src = cv.imread('canvas');
-    cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
+    let gray_src = new cv.Mat();
+    cv.cvtColor(src, gray_src, cv.COLOR_RGB2GRAY, 0);
+    let raw_edge = new cv.Mat();
     let edge = new cv.Mat();
-    // You can try more different parameters
-    cv.Canny(src, edge, 50, 100, 3, false);
 
+    // You can try more different parameters
+    cv.Canny(gray_src, raw_edge, 100, 200);
+    let M = cv.Mat.ones(5, 5, cv.CV_8U);
+    // You can try more different parameters
+    cv.morphologyEx(raw_edge, edge, cv.MORPH_CLOSE, M);
+    
     let dst = cv.Mat.zeros(edge.rows, edge.cols, cv.CV_8UC3);
     cv.threshold(edge, edge, 100, 200, cv.THRESH_BINARY);
     let contours = new cv.MatVector();
     let hierarchy = new cv.Mat();
     let poly = new cv.MatVector();
     cv.findContours(edge, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+
     // approximates each contour to polygon
+    let maxContourArea = 0;
+    let maxContourIndex = -1;
     for (let i = 0; i < contours.size(); ++i) {
         let tmp = new cv.Mat();
         let cnt = contours.get(i);
-        // You can try more different parameters
-        cv.approxPolyDP(cnt, tmp, 3, true);
-        poly.push_back(tmp);
-        cnt.delete(); tmp.delete();
+        if (cv.contourArea(cnt) > 10000) {
+            if (cv.contourArea(cnt) > maxContourArea) {
+                maxContourArea = cv.contourArea(cnt);
+                maxContourIndex = i;
+            }
+            // You can try more different parameters
+            let perimeter = cv.arcLength(cnt, true) * 0.005;
+            if (perimeter < 8) {
+                perimeter = 8;
+            }
+            cv.approxPolyDP(cnt, tmp, perimeter, true);
+            poly.push_back(tmp);
+            cnt.delete(); tmp.delete();
+        }
     }
-    // draw contours with random Scalar
-    for (let i = 0; i < contours.size(); ++i) {
-        let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255),
-            Math.round(Math.random() * 255));
-        cv.drawContours(dst, poly, i, color, 1, 8, hierarchy, 0);
+    if (maxContourIndex >= 0) {
+        let rotatedRect = cv.minAreaRect(contours.get(maxContourIndex));
+        let vertices = cv.RotatedRect.points(rotatedRect);
+        let contoursColor = new cv.Scalar(255, 255, 255, 100);
+        let rectangleColor = new cv.Scalar(255, 0, 0, 222);
+        cv.drawContours(src, contours, maxContourIndex, contoursColor, 1, 8, hierarchy, 100);
+        // draw rotatedRect
+        for (let i = 0; i < 4; i++) {
+            cv.line(src, vertices[i], vertices[(i + 1) % 4], rectangleColor, 2, cv.LINE_AA, 0);
+        }
+    //    cv.drawContours(dst, poly, i, color, 1, 8, hierarchy, 0);
+        cv.imshow('canvasOutput', src);
     }
-    cv.imshow('canvasOutput', dst);
     src.delete(); edge.delete(); dst.delete(); hierarchy.delete(); contours.delete(); poly.delete();
 }
+
