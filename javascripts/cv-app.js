8903bd7bff62b8c57704eb05abe0bd2c1914e92e
shenzt <shenzt@gmail.com>
2020-12-27 15:54:46 +0000

    Deploying to gh-pages from  @ ab54ad0357cf47777d212361baa93c0ba91d3293 🚀

--- javascripts/cv-app.js
+++ javascripts/cv-app.js
@@ -1,16 +1,17 @@
 function Cov() {
     let src = cv.imread('canvas');
     cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
+    let edge = new cv.Mat();
     // You can try more different parameters
-    cv.Canny(src, src, 50, 100, 3, false);
+    cv.Canny(src, edge, 50, 100, 3, false);
 
-    let dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
-    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
-    cv.threshold(src, src, 100, 200, cv.THRESH_BINARY);
+    let dst = cv.Mat.zeros(edge.rows, edge.cols, cv.CV_8UC3);
+    cv.cvtColor(edge, edge, cv.COLOR_RGBA2GRAY, 0);
+    cv.threshold(edge, edge, 100, 200, cv.THRESH_BINARY);
     let contours = new cv.MatVector();
     let hierarchy = new cv.Mat();
     let poly = new cv.MatVector();
-    cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+    cv.findContours(edge, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
     // approximates each contour to polygon
     for (let i = 0; i < contours.size(); ++i) {
         let tmp = new cv.Mat();
@@ -27,5 +28,5 @@ function Cov() {
         cv.drawContours(dst, poly, i, color, 1, 8, hierarchy, 0);
     }
     cv.imshow('canvasOutput', dst);
-    src.delete(); dst.delete(); hierarchy.delete(); contours.delete(); poly.delete();
+    src.delete(); edge.delete(); dst.delete(); hierarchy.delete(); contours.delete(); poly.delete();
 }
