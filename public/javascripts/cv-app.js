2fecbecdb7147ce7ab635813ad580387681daac2
Zhitao Shen <shenzt@gmail.com>
2021-01-04 01:10:04 +0800

    improve cv algorithm

--- public/javascripts/cv-app.js
+++ public/javascripts/cv-app.js
@@ -6,15 +6,20 @@ function Cov() {
         cv.cvtColor(src, gray_src, cv.COLOR_RGB2GRAY, 0);
         let raw_edge = new cv.Mat();
         let edge = new cv.Mat();
+        let ksize = new cv.Size(5, 5);
+        cv.GaussianBlur(gray_src, gray_src, ksize, 0, 0, cv.BORDER_DEFAULT);
 
         // You can try more different parameters
         cv.Canny(gray_src, raw_edge, 100, 200);
-        let M = cv.Mat.ones(5, 5, cv.CV_8U);
+        let kernel_size = Math.ceil(Math.min(raw_edge.cols, raw_edge.rows)/144);
+        let M = cv.Mat.ones(kernel_size, kernel_size, cv.CV_8U);
         // You can try more different parameters
         cv.morphologyEx(raw_edge, edge, cv.MORPH_CLOSE, M);
-        
         let dst = cv.Mat.zeros(edge.rows, edge.cols, cv.CV_8UC3);
-        cv.threshold(edge, edge, 100, 200, cv.THRESH_BINARY);
+        cv.adaptiveThreshold(edge, edge, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, kernel_size, 2);
+
+//        cv.threshold(edge, edge, 100, 200, cv.THRESH_BINARY);
+
         let contours = new cv.MatVector();
         let hierarchy = new cv.Mat();
         let poly = new cv.MatVector();
@@ -52,12 +57,19 @@ function Cov() {
                 cv.line(src, vertices[i], vertices[(i + 1) % 4], rectangleColor, 2, cv.LINE_AA, 0);
             }
 
-            // try{
+            let angle = rotatedRect.angle;
+            let center = new cv.Point(src.cols / 2, src.rows / 2);
+
+            M = cv.getRotationMatrix2D(center,angle,1);
+            let dsize = new cv.Size(src.rows*2, src.cols*2);
+            cv.warpAffine(src, src, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+        
+            //try{
             //     let rect = new cv.Rect(rotatedRect.center.x/2, rotatedRect.center.y/2, rotatedRect.size.width, rotatedRect.size.height);
             //     src = src.roi(rect);
-            // }catch(e){
-            //     console.error(e, rotatedRect.center.x/2, rotatedRect.center.y/2, rotatedRect.size.width, rotatedRect.size.height);
-            // }
+             //}catch(e){
+             //    console.error(e, rotatedRect.center.x/2, rotatedRect.center.y/2, rotatedRect.size.width, rotatedRect.size.height);
+             //}
 
         //    cv.drawContours(dst, poly, i, color, 1, 8, hierarchy, 0);
             cv.imshow('canvasOutput', src);
